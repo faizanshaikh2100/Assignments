f8f67ecb22c1420a1ec36dab1ed2809e4d0973b7
unknown <faizanshaikh2100@gmail.com>
2023-05-22 09:20:18 +0530

    added commit and rollback

---
+++ src/MYSQL/commit_rollback.sql
@@ -0,0 +1,15 @@
+-- Set autocommit = off;
+
+-- select productName from products;
+
+-- commit;
+
+-- delete from departments;
+
+
+-- select * from departments;
+
+
+-- rollback;
+select * from departments;
+
